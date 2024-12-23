be88f68af8c1013081fb143c927cd26ea1c233a6
avishek100 <avishek.chaudhary1002@gmail.com>
2024-12-23 21:06:43 +0545

    credentials model completed

---
+++ model/Credential.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const credSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true },
+});
+
+const Cred = mongoose.model('Cred', credSchema);
+module.exports = Cred;
