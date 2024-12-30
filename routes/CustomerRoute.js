279b3ce150b7eb3a93bb7d67eac812a52cb1e516
avishek100 <avishek.chaudhary1002@gmail.com>
2024-12-30 23:55:38 +0545

    customers route completed

--- routes/CustomerRoute.js
+++ routes/CustomerRoute.js
@@ -1,15 +1,15 @@
-// const express = require("express")
-// const router = express.Router();
-// const { findAll, save, findById, deleteById, update } = require("../controller/CustomerController");
-// const CustomerValidation = require("../validation/CustomerValidation")
+const express = require("express")
+const router = express.Router();
+const { findAll, save, findById, deleteById, update } = require("../controller/CustomerController");
+const CustomerValidation = require("../validation/CustomerValidation")
 
 
 
 
-// router.get("/", findAll);
-// router.post("/", CustomerValidation, save);
-// router.get("/:id", findById);
-// router.delete("/:id", deleteById);
-// router.put("/:id", update);
+router.get("/", findAll);
+router.post("/", CustomerValidation, save);
+router.get("/:id", findById);
+router.delete("/:id", deleteById);
+router.put("/:id", update);
 
-// module.exports = router;
+module.exports = router;
