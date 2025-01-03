81897c22d743084784527793f71a7e3cd9be3d97
avishek100 <avishek.chaudhary1002@gmail.com>
2025-01-04 00:20:03 +0545

    video model completed

---
+++ model/Video.js
@@ -0,0 +1,10 @@
+// const mongoose = require("mongoose");
+//
+// const videoSchema = new mongoose.Schema({
+//     title: { type: String, required: true },
+//     description: { type: String },
+//     videoPath: { type: String, required: true },
+//     uploadDate: { type: Date, default: Date.now },
+// });
+//
+// module.exports = mongoose.model("Video", videoSchema)
