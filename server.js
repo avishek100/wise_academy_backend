60619601e226177ccc85b64eb3ee02eb813a6893
avishek100 <avishek.chaudhary1002@gmail.com>
2025-02-28 23:59:24 +0545

    updated

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
-// 
+
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
